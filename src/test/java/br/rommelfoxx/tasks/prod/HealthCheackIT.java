b70f604fc34ce17cdd34ffd5d5764a96ad1710e2
vanderlan <vand>
2020-04-30 13:34:40 -0300

    Produção

---
+++ src/test/java/br/rommelfoxx/tasks/prod/HealthCheackIT.java
@@ -0,0 +1,36 @@
+package br.rommelfoxx.tasks.prod;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import junit.framework.Assert;
+
+public class HealthCheackIT {
+
+	@Test
+	public void healthCheck() throws MalformedURLException{
+		
+		DesiredCapabilities cap = DesiredCapabilities.chrome();
+		WebDriver driver = new RemoteWebDriver(new URL("http://192.168.0.183:4444/wd/hub"),cap);
+		try {
+			
+		driver.navigate().to("http://192.168.0.183:9999/tasks/");
+		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);	
+		String version = driver.findElement(By.id("version")).getText();
+		System.out.println(version);
+		Assert.assertTrue(version.startsWith("build"));
+		}
+		
+		finally {
+			driver.quit();
+		}
+	}
+	
+}
